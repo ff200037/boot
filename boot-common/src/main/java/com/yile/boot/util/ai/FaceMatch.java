0791ec675614047cd4cff253997fb1971a26be26
ff200037 <ff200037@126.com>
2018-12-17 10:56:46 +0800

    增加工具类

--- boot-common/src/main/java/com/yile/boot/util/ai/FaceMatch.java
+++ boot-common/src/main/java/com/yile/boot/util/ai/FaceMatch.java
@@ -1,6 +1,5 @@
 package com.yile.boot.util.ai;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -11,22 +10,18 @@ import com.yile.boot.util.ai.util.Base64Util;
 import com.yile.boot.util.ai.util.FileUtil;
 import com.yile.boot.util.ai.util.GsonUtils;
 import com.yile.boot.util.ai.util.HttpUtil;
+import com.yile.boot.util.ai.util.ImageEnums.FaceType;
 
 import net.sf.json.JSONObject;
 
-/**
- * 百度AI人脸识别
- * @author Administrator
- *
- */
 public class FaceMatch {
 	
-	public static String match(String filePath1, String filePath2) {
+	public static String match(String filePath1, String filePath2, String faceType1, String faceType2) {
 		try {
 			byte[] bytes1 = FileUtil.readFileByBytes(filePath1);
 	        byte[] bytes2 = FileUtil.readFileByBytes(filePath2);
 	        
-	        String result = matchImage(bytes1, bytes2);
+	        String result = matchImage(bytes1, bytes2, faceType1, faceType2);
 	        return result;
 		} catch (Exception e) {
             e.printStackTrace();
@@ -34,10 +29,10 @@ public class FaceMatch {
         return null;
     }
 	
-	public static Double getMatchScore(String filePath1, String filePath2){
+	public static double getMatchScore(String filePath1, String filePath2, String faceType1, String faceType2){
 		double score = 0.0;        
 		
-        String result = match(filePath1, filePath2);
+        String result = match(filePath1, filePath2, faceType1,  faceType2);
         
         JSONObject jsonObject =JSONObject.fromObject(result);
         int errorCode = jsonObject.getInt("error_code");
@@ -53,10 +48,10 @@ public class FaceMatch {
         return score;
     }
 	
-	public static Double getMatchScore(byte[] bytes1, byte[] bytes2){
+	public static double getMatchScore(byte[] bytes1, byte[] bytes2){
 		double score = 0.0;
 		        
-        String result = matchImage(bytes1, bytes2);
+        String result = matchImage(bytes1, bytes2, FaceType.LIVE.getValue(), FaceType.LIVE.getValue());
         JSONObject jsonObject =JSONObject.fromObject(result);
         int errorCode = jsonObject.getInt("error_code");
         if(errorCode == 0){
@@ -71,7 +66,25 @@ public class FaceMatch {
         return score;
     }	
 	
-	public static String matchImage(byte[] bytes1, byte[] bytes2) {
+	public static double getMatchScore(byte[] bytes1, byte[] bytes2, String faceType1, String faceType2){
+		double score = 0.0;
+		        
+        String result = matchImage(bytes1, bytes2, faceType1, faceType2);
+        JSONObject jsonObject =JSONObject.fromObject(result);
+        int errorCode = jsonObject.getInt("error_code");
+        if(errorCode == 0){
+        	JSONObject resultObj = jsonObject.getJSONObject("result");
+            score = resultObj.getDouble("score");
+        } else {
+        	String errorMsg = jsonObject.getString("error_msg");
+        	System.out.println("errorMsg: " + errorMsg);
+        	throw new RuntimeException(errorMsg);
+        }
+        
+        return score;
+    }
+	
+	public static String matchImage(byte[] bytes1, byte[] bytes2, String faceType1, String faceType2) {
         // 请求url
         String url = "https://aip.baidubce.com/rest/2.0/face/v3/match";
         try {
@@ -83,14 +96,14 @@ public class FaceMatch {
             Map<String, Object> map1 = new HashMap<>();
             map1.put("image", image1);
             map1.put("image_type", "BASE64");
-            map1.put("face_type", "LIVE");
+            map1.put("face_type", faceType1);
             map1.put("quality_control", "NONE");
             map1.put("liveness_control", "NONE");
 
             Map<String, Object> map2 = new HashMap<>();
             map2.put("image", image2);
             map2.put("image_type", "BASE64");
-            map2.put("face_type", "LIVE");
+            map2.put("face_type", faceType2);
             map2.put("quality_control", "NONE");
             map2.put("liveness_control", "NONE");
 
@@ -108,12 +121,7 @@ public class FaceMatch {
     }
 
     public static void main(String[] args) throws Exception {
-    	File file = new File("d:\\p1.jpg");
-    	System.out.println(file);
-    	byte[] byte1 = FileUtil.readFileByBytes(file);
-    	byte[] byte2 = FileUtil.readFileByBytes(file);
-    	 
-    	double score = FaceMatch.getMatchScore(byte1, byte2);
+    	double score = FaceMatch.getMatchScore("d:\\d1.jpg", "d:\\d2.jpg", FaceType.LIVE.getValue(), FaceType.CERT.getValue());
     	System.out.println(score);
     }
 }
